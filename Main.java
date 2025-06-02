3d249f391e170dd4275922cd367c6d0d6153b3c5
mr_tilin <diegoalexandergonzalez62@gmail.com>
2025-06-02 11:47:38 -0600

    resuleto

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Consulta consulta1 = new Consulta("rojo.json");
+        Consulta consulta2 = new Consulta("verde.json");
+        Consulta consulta3 = new Consulta("amarillo.json");
+        Consulta consulta4 = new Consulta(".json");
+    }
+}
